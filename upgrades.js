dd7bf30a4eb12d1805fe4f1bc64d350605b65312
Glitch (hello-webpage) <none>
2021-02-14 05:02:27 +0000

    🍠🛤 Checkpoint ./upgrades.js:23678182/444:25044483/389 ./index.html:25044483/7

--- upgrades.js
+++ upgrades.js
@@ -4,7 +4,7 @@ function getCost(x, data = game) {
       return D.pow(5, data.rebuyables[11]).mul(0.01)
     },
     12() {
-      
+      return D.pow(10, data.rebuyables[12]).mul(0.1) // should 1 be the base tho
     }
   }
   return upgradeCosts[x]()
@@ -20,4 +20,17 @@ function getEffect(x, data = game) {
     }
   }
   return upgradeEffects[x]()
-} // wow we have a separate thing now gflghjflkhfglhjfglhkjfgl thonkeres tonogvjnhkgfwers
+} // wow we have a separate thing now gflghjflkhfglhjfglhkjfgl thonkeres thonkeres
+
+function buy(x) {
+  let buyFuncs = {
+    11() {
+      if (game.points.gte(getCost(11))) {
+        // game.points = game.points.sub(getCost(11)) // does this work????? thonkeres thonkeres
+        game.rebuyables[11] = game.rebuyables[11].add(1)
+      }
+    }
+  }
+    buyFuncs[x]()
+    // im asking di server what they think // ok
+}
